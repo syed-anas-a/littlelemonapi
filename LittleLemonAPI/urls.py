9dde1ca07f18732190596c0fde377d342f87a622
syed-anas-a <syedanasa86@gmail.com>
2026-02-10 18:19:24 +0530

    token view

--- LittleLemonAPI/urls.py
+++ LittleLemonAPI/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path, include
 from . import views
+# from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     #User registration and token generation endpoints 
-    path('', include('djoser.urls')),
-    path('', include('djoser.urls.authtoken')),
+    path('api/', include('djoser.urls')),
+    path('api/', include('djoser.urls.authtoken')),
 
     #MenuItemview
-    path('menu-items/', views.MenuItemListCreate.as_view())
+    path('menu-items/', views.MenuItemListCreate.as_view()),
+    path('menu-items/<str:item>', views.MenuItemRUD.as_view())
 ]
--- LittleLemonAPI/views.py
+++ LittleLemonAPI/views.py
@@ -2,12 +2,24 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import MenuItem
 from .serializers import MenuItemSerializer
+from rest_framework.permissions import IsAdminUser, IsAuthenticatedOrReadOnly
 
 # Create your views here.
 
 class MenuItemListCreate(generics.ListCreateAPIView):
     queryset = MenuItem.objects.all()
     serializer_class = MenuItemSerializer 
+
+    def get_permissions(self):
+        if(self.request.method == 'POST'):
+            return [IsAdminUser()]
+        else:
+            [IsAuthenticatedOrReadOnly()]
+
+class MenuItemRUD(generics.RetrieveUpdateDestroyAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+    lookup_field = 'item'
 
     
 
